ef478251dabe5fe1bb52006a6c6337dff3623564
Rohan Minocha <rohan.minocha2002@gmail.com>
2021-06-21 19:36:02 -0400

    Create web.go

---
+++ routes/web.go
@@ -0,0 +1,17 @@
+ackage routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/rohanxminocha/to-do-list/controllers"
+)
+
+func Init() *mux.Router {
+	route := mux.NewRouter()
+
+	route.HandleFunc("/", controllers.Show)
+	route.HandleFunc("/add", controllers.Add).Methods("POST")
+	route.HandleFunc("/delete/{id}", controllers.Delete)
+	route.HandleFunc("/complete/{id}", controllers.Complete)
+
+	return route
+}
